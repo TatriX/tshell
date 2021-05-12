fd567557454370d185a1598ffd126014be63534c
TatriX <tatrics@gmail.com>
2021-05-12 21:54:53 +0200

    feat: Add package related info and require transient

--- tshell.el
+++ tshell.el
@@ -1,5 +1,33 @@
+;;; tshell.el --- Experimental alternative shell for Emacs
 ;; -*- lexical-binding: t -*-
 
+;; Author: TatriX <tatrics@gmail.com>
+;; URL: https://github.com/TatriX/pomidor
+;; Keywords: terminals, shell
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.3") (transient "0.3"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; tshell is an experimental buffer-oriented Emacs shell.
+;; See https://github.com/TatriX/tshell for documentation.
+
+;;; Code:
+
+(require 'transient)
 (defvar tshell-buffer "*tshell*")
 (defvar tshell-out-buffer "*tshell-out*")
 
